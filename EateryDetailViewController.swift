39c46fa03d9f2ee7b28d4baf8356cf90532d9465
maksikgo <98282080+maksikgo@users.noreply.github.com>
2022-01-30 13:09:45 +0300

    maps features with maps adding

--- EateryDetailViewController.swift
+++ EateryDetailViewController.swift
@@ -10,24 +10,25 @@ import UIKit
 class EateryDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // Return the number of rows for the table.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 0
+        return 0
     }
-
+    
     // Provide a cell object for each row.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       // Fetch a cell of the appropriate type.
-       let cell = tableView.dequeueReusableCell(withIdentifier: "cellTypeIdentifier", for: indexPath)
-       
-       // Configure the cell’s contents.
-       cell.textLabel!.text = "Cell text"
-           
-       return cell
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellTypeIdentifier", for: indexPath)
+        
+        // Configure the cell’s contents.
+        cell.textLabel!.text = "Cell text"
+        
+        return cell
     }
     
     
     @IBOutlet weak var rateButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var mapButton: UIButton!
     
     @IBAction func unwindSegue(segue: UIStoryboardSegue) {
         guard let svc = segue.source as? RateViewController else { return }
@@ -46,49 +47,61 @@ class EateryDetailViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        rateButton.layer.cornerRadius = 5
-        rateButton.layer.borderWidth = 1
-        rateButton.layer.borderColor = UIColor.white.cgColor
+        let buttons = [rateButton, mapButton]
+        for button in buttons {
+            guard let button = button else { break }
+            button.layer.cornerRadius = 5
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.white.cgColor
+        }
+        
         imageView.image = UIImage(named: restaurant!.image)
         title = restaurant!.name
         
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EateryDetailTableViewCell
-        
-        switch indexPath.row {
-        case 0:
-            cell.keyLabel.text = "Name"
-            cell.valueLabel.text = restaurant!.name
-        case 1:
-            cell.keyLabel.text = "Type"
-            cell.valueLabel.text = restaurant!.type
-        case 2:
-            cell.keyLabel.text = "Adress"
-            cell.valueLabel.text = restaurant!.location
-        case 3:
-            cell.keyLabel.text = "I was here"
-            cell.valueLabel.text = restaurant!.isVisited ? "Yes" : "No"
-            
-        default:
-            break
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
         }
         
-        cell.backgroundColor = UIColor.clear
-        return cell
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 4
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EateryDetailTableViewCell
+            
+            switch indexPath.row {
+            case 0:
+                cell.keyLabel.text = "Name"
+                cell.valueLabel.text = restaurant!.name
+            case 1:
+                cell.keyLabel.text = "Type"
+                cell.valueLabel.text = restaurant!.type
+            case 2:
+                cell.keyLabel.text = "Adress"
+                cell.valueLabel.text = restaurant!.location
+            case 3:
+                cell.keyLabel.text = "I was here"
+                cell.valueLabel.text = restaurant!.isVisited ? "Yes" : "No"
+                
+            default:
+                break
+            }
+            
+            cell.backgroundColor = UIColor.clear
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "mapSegue" {
+                let dvc = segue.destination as! MapViewController
+                dvc.restaurant = self.restaurant
+                
+            }
+        }
+        
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-}
-
 }
